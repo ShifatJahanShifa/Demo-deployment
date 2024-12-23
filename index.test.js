ea2a2c21b3922102ed09cacc2782e6ecec12e978
ShifatJahanShifa <bsse1301@iit.du.ac.bd>
2024-12-23 19:36:18 +0600

    fixed the bug

--- index.test.js
+++ index.test.js
@@ -1,9 +1,21 @@
 const request = require('supertest');
 const app = require('./index'); // Import the app
+const http = require('http');
+
+let server;
+
+beforeAll((done) => {
+    server = http.createServer(app);
+    server.listen(4000, done);
+});
+
+afterAll((done) => {
+    server.close(done);
+});
 
 describe('GET /', () => {
     it('should return Hello World', async () => {
-        const response = await request(app).get('/');
+        const response = await request(server).get('/');
         expect(response.status).toBe(200);
         expect(response.text).toBe('Hello World');
     });
